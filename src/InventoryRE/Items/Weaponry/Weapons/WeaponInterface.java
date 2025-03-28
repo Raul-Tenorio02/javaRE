ff914a0ceb27f0d77505561f1bcc2ed7256e1508
Raul Tenorio <raul.amaral2005@gmail.com>
2025-03-28 10:27:20 -0300

    refactor

--- src/InventoryRE/Items/Weaponry/Weapons/WeaponInterface.java
+++ src/InventoryRE/Items/Weaponry/Weapons/WeaponInterface.java
@@ -1,11 +1,13 @@
 package InventoryRE.Items.Weaponry.Weapons;
 
 import InventoryRE.Items.Weaponry.Ammunition.Ammunition;
+import InventoryRE.Items.Weaponry.WeaponParts.Parts;
 
 public interface WeaponInterface {
 
     ReloadRecord reloadWeapon(Ammunition ammo);
     Ammunition unloadAmmo(); // specific for grenade launchers
-    void fireCount(int count);
+    void weaponUseCount(int count);
+    Weapon upgradeWeapon(Parts part);
 
 }
